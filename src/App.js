af534cafea7c789ad3b7c5babe912f72f801b0d8
AbdoShady <abdoshady550@gmail.com>
2023-10-28 01:46:15 +0200

    a laud animation for wait api work

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ function App() {
   }, []);
 
   return (
+    
     <div className="App">
       <NavBar search={search} />
       <Container >
--- src/componant/CardsList.js
+++ src/componant/CardsList.js
@@ -2,16 +2,16 @@ import React from 'react'
 import Items from './Items'
 import { Row } from 'react-bootstrap'
 import  Pagination  from './Pagination';
+import { Spinner } from 'react-spinner-animated';
 const CardsList = ({ movies,getPage,pageCount }) => {
 
   return (
 
     <Row className="my-4 px-5 p-4 content-style">
-      {movies.length >= 1 ? (movies.map((mov) => {
+      {movies.length >=1 ? (movies.map((mov) => {
         return (<Items key={mov.id} mov={mov} />)
       })) 
-      : (<div style={{height:'20vh', backgroundColor: 'white', marginTop: '50px' }}>
-         <h1 style={{ color: '#000304', textAlign: 'center', fontSize: '50px',marginTop: '50px' }}> Oops ! NOT Found 🤷</h1></div>)}
+      : (<Spinner  center={true} width={"100px"} height={"300px"}/>)}
          {movies.length >= 1 ?( <Pagination pageCount={pageCount}  getPage={getPage}/> ):null}   
     </Row>
   )
